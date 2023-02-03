150ab3dd2d687200bcc90249d2bbe662c7571279
Frank <f.kinuthia01@gmail.com>
2023-02-04 01:40:01 +0300

    Arrow function practice update run

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-function distanceFromHqInBlocks(distance) {
-  let pickupLocation = distance > 42 ? distance - 42 : 42 - distance
-  return pickupLocation;
+
+const distanceFromHqInBlocks = (distance) => {
+	let pickupLocation = distance > 42? distance - 42 : 42 - distance;
+	return pickupLocation;
 }
 
-function distanceFromHqInFeet(pickupLocation) {
-  let difference = distanceFromHqInBlocks(pickupLocation) ;
-  return difference * 264;
-}
-function distanceTravelledInFeet(start, destination){
-  let travel = destination > start ? (destination - start ) * 264 :( start - destination) * 264 
-  return travel;
-  
+
+const distanceFromHqInFeet = (pickupLocation) => {
+	let difference = distanceFromHqInBlocks(pickupLocation) ;
+	return difference * 264;
 }
 
-function calculatesFarePrice(start, destination) {
-	let pricing = distanceTravelledInFeet(start, destination);
-	if(pricing <= 400) {
-		return 0;
+
+const distanceTravelledInFeet = (start, destination) => {
+	let travel = destination > start? (destination - start ) * 264 :( start - destination) * 264 
+    return travel;
+}
+
+
+
+const calculatesFarePrice = (start, destination) => {
+    let pricing = distanceTravelledInFeet(start, destination);
+    if(pricing <= 400) {
+        return 0;
 	} else if(pricing > 400 && pricing < 2000) {
-		return 2.56
-	} else if(pricing > 2000) {
-		if(pricing > 2500) {
-			return `cannot travel that far`;
-		}
-		return 25;
+		return 2.56;
 	}
+	else if (pricing > 2000) {
+		if(pricing > 2500) {
+            return `cannot travel that far`;
+        }
+        return 25;
+    }
 }
 
